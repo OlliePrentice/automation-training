9bf4c010778741b03ccc34aae1d4d3ab8cb5e071
Ollie Prentice <osprentice@gmail.com>
2022-05-16 18:12:54 +0100

    Opens sky.com and closes cookie notice

---
+++ index.js
@@ -0,0 +1,29 @@
+require("chromedriver");
+const { Builder, By, until } = require("selenium-webdriver");
+
+const webpage = async () => {
+    const driver = new Builder().forBrowser("chrome").build();
+
+    await driver.get("https://sky.com");
+
+    try {
+        const noticeFrame = await driver.findElement(
+            By.xpath("//*[@id='sp_message_iframe_474555']")
+        );
+
+        await driver.switchTo().frame(noticeFrame);
+
+        let button = await driver.wait(
+            until.elementLocated(
+                By.xpath("//*[@id='notice']/div[3]/button[2]")
+            ),
+            1000
+        );
+
+        await button.click();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+webpage();
